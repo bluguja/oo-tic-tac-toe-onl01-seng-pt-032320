e262aa9d6a285873ed22ebc4d2b9d255a6948bd1
Bala Luguja <bluguja@gmail.com>
2020-04-22 01:24:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,14 @@
 class TicTacToe
+  
+  
   def initialize
-    @board = Array.new =(9," ")
+    @cells = Array.new =(9," ")
 end
+
+def display_board
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
