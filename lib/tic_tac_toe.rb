4b763de5a59c48efdfb953af095bcac6f5a766e0
Bala Luguja <bluguja@gmail.com>
2020-04-22 01:34:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,14 +1,21 @@
 class TicTacToe
- attr_accessor :cells 
+ attr_accessor :cells
+ win_combinations =[
+   [0,1,2]
+   [3,4,5]
+   [6,7,8]
+   
+   ]
   
   def initialize
     @cells = Array.new =(9," ")
-end
+  end
 
-def display_board
+  def display_board
     puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
     puts "-----------"
     puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
     puts "-----------"
     puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
+end
